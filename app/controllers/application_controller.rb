91ec84ecc3cbd0065eca720604c5b620b9f7af9e
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-04 18:53:54 +0900

    ストロングパラメーター、ログイン必須

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :cofigure_permitted_parameters, if: :devise_controller?
+
+  private
+  def cofigure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])    
+  end
 end
