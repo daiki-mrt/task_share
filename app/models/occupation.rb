4b4af7d9d02d28783037fb4f02d536fb4e060c37
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-05 11:33:29 +0900

    occupationモデル作成

---
+++ app/models/occupation.rb
@@ -0,0 +1,12 @@
+class Occupation < ActiveHash::Base
+
+  self.data = [
+    {id: 0, name: "選択して下さい"},
+    {id: 1, name: "会社員"},
+    {id: 2, name: "自営業"},
+    {id: 3, name: "アルバイト・パート"},
+    {id: 4, name: "学生"},
+    {id: 5, name: "その他"}
+  ]
+  
+end
