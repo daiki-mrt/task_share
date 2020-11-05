eb9b1735a319b05ed95fe950acdfc5966ec1f256
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-05 18:28:20 +0900

    ユーザー詳細をshowアクションで表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'users#index'
-  get '/', to: 'users#index'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+
+  resources :users, only: [:index, :show]
 end
