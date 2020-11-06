19ae03e339d19d0ceed73383ad8a04625941bb97
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-06 17:56:48 +0900

    ログイン・ログアウト後のページ遷移

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,15 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  # ログイン後にマイページに遷移する
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
+  # ログアウト後にログイン画面に遷移する
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
   # GET /resource/sign_in
   # def new
   #   super
