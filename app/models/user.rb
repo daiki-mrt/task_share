ffa6a14df618313c03cc9f156576f82503a0110f
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-07 09:21:33 +0900

    アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,11 @@ class User < ApplicationRecord
   has_one :profile
   accepts_nested_attributes_for :profile, update_only: true
   has_many :tasks
+
+  # フォロー側
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id, dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :follower
+  # フォロワー側
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :following
 end
