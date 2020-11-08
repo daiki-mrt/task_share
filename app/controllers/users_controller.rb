35b392395121e2d3a22c5f2a02a08b4cb833bf2c
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-08 18:23:22 +0900

    DMページへ遷移

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,18 @@ class UsersController < ApplicationController
     @tasks = Task.where(user_id: @user.id).order("created_at DESC")
     @following_users = @user.followings
     @follower_users = @user.followers
+
+
+    # DM用するボタンのためのデータ取得
+    # user_roomから自分が含まれるidを取得
+    room_ids = current_user.user_rooms.pluck(:room_id)
+    # 取得したuser_roomのうち、自分と相手のペアを探す
+    @target_user_room = UserRoom.find_by(room_id: room_ids, user_id: @user.id)    
+    # 特定したuser_roomからroom_idを取得する
+    if @target_user_room.present?
+      @room = @target_user_room.room
+    end
+
   end
 
   # フォロー数
