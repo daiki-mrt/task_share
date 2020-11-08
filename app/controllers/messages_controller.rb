2ada8c6fbac4862f9004aae18d17012b7927dd46
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-08 18:23:30 +0900

    メッセージの投稿

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,15 @@
 class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
-    binding.pry
     if @message.save
-      redirect_to room_path(params[:room_id])
+      redirect_to room_path(params[:message][:room_id])
     else
-      redirect_to room_path(params[:room_id])
+      redirect_to room_path(params[:message][:room_id])
     end
   end
 
   private
   def message_params
-    params.require(:message).permit(:text).merge(room_id: params[:room_id], user_id: current_user.id)
+    params.require(:message).permit(:text, :room_id).merge(user_id: current_user.id)
   end
 end
