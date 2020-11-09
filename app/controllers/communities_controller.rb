d56fa3fe98b717c92f857b7fafd8cea59dfd9908
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-09 16:41:21 +0900

    community作成と一覧表示

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,5 +1,23 @@
 class CommunitiesController < ApplicationController
   def index
-    
+    @communities = Community.all
+  end
+
+  def new
+    @community = Community.new
+  end
+
+  def create
+    @community = Community.new(community_params)
+    if @community.save
+      redirect_to communities_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def community_params
+    params.require(:community).permit(:name, :category_id, :text).merge(user_id: current_user.id)
   end
 end
