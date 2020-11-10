0af392e562f854fd2b7976ff4a56a8a8fbaac32a
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-10 11:39:21 +0900

    chatの投稿と表示

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,2 +1,32 @@
 class ChatsController < ApplicationController
+  before_action :set_community, only: [:index, :create]
+  before_action :set_chats, only: [:index, :create]
+  
+  def index
+    set_community
+    set_chats
+    @chat = Chat.new
+  end
+
+  def create
+    @chat = Chat.new(chat_params)
+    if @chat.save
+      redirect_to community_chats_path(@community)
+    else      
+      render :index
+    end
+  end
+
+  private
+  def set_community
+    @community = Community.find(params[:community_id])
+  end
+
+  def set_chats
+    @chats = Chat.includes(:user)
+  end
+
+  def chat_params
+    params.require(:chat).permit(:text).merge(user_id: current_user.id, community_id: params[:community_id])
+  end
 end
