4f80362ab909759a8c80336c34a324382a969d2b
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-10 18:25:55 +0900

    questionの投稿と一覧表示の実装

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,34 @@
+class QuestionsController < ApplicationController
+  before_action :set_community
+  before_action :set_questions
+  
+  def index
+    set_community
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to community_questions_path(@community)
+    else
+      render :new
+    end
+  end
+
+  private
+  def set_community
+    @community = Community.find(params[:community_id])
+  end
+
+  def set_questions
+    @questions = @community.questions.includes(:user)
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :content).merge(user_id: current_user.id, community_id: params[:community_id])
+  end
+end
