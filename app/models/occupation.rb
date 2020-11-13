a80d0082cfeca416e7d26ea200cf22b97fe56467
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-13 19:27:40 +0900

    職種がid=0で登録される不具合を解消

--- app/models/occupation.rb
+++ app/models/occupation.rb
@@ -1,7 +1,7 @@
 class Occupation < ActiveHash::Base
 
   self.data = [
-    {id: 0, name: "選択して下さい"},
+    # {id: 0, name: "選択して下さい"},
     {id: 1, name: "会社員"},
     {id: 2, name: "自営業"},
     {id: 3, name: "アルバイト・パート"},
