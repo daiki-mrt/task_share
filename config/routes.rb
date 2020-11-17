8a77205fe327a3baa78280735455f1eb052ebda4
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-17 23:02:29 +0900

    ゲストログインの作成 (検索)メソッドを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_scope :user do
     get '/', to: 'users/sessions#new'
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
   devise_for :users, controllers: {
     registrations: 'users/registrations',
