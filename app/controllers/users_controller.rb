2b09260ed6c292c615b108eed5d0f61076404fe7
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-17 18:29:16 +0900

    マイページで完了していないタスクを取得

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
   def show
     set_user
-    @tasks = Task.where(user_id: @user.id).order("created_at DESC")
+    @tasks = Task.user_is(@user.id).not_completed.order("created_at DESC")
     @following_users = @user.followings
     @follower_users = @user.followers
 
