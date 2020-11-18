04618dc93c36cc946a1120cf277f8b8a609f308d
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-18 16:32:39 +0900

    RSpecの初期設定

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,16 @@ module TaskShare
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
 
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: false,
+                       system_specs: false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
