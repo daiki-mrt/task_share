ec623f92ee1cabc20f53f1474152c298738f5d9d
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-19 11:12:30 +0900

    passwordの生成設定を変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name {Faker::Name.name}
     email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
+    password {"abc123"}
     password_confirmation {password}
   end
 end
