f38952eccd77b784262856c03d9ca0843fa3bd4e
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-20 16:57:04 +0900

    メソッド間の空行を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -73,6 +73,7 @@ class TasksController < ApplicationController
   def set_task
     @task = Task.find(params[:id])
   end
+
   def task_params
     params.require(:task).permit(:title, :text).merge(user_id: current_user.id)
   end
