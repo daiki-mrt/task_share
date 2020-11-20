a278907f4ac5ade21fded5e7ac5aa90eacf647cc
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-20 21:43:45 +0900

    Style/RedundantRegexpCharacterClass

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: PASSWORD_REGEX, message: "は半角英数字で入力してください" }
 
   has_one :profile, dependent: :destroy
