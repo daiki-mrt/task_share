4b6a48667c44d3d7979b6e294af32cc8e8474da4
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-25 23:24:50 +0900

    検索メソッドのテスト

--- spec/factories/communities.rb
+++ spec/factories/communities.rb
@@ -6,23 +6,30 @@ FactoryBot.define do
     association :user
   end
 
-  factory :community_category2 do
+  factory :community_category2, class:"Community" do
     sequence(:name) { |n| "コミュニティ名_#{n}" }
     text { "コミュニティの紹介文" }
     category_id { 2 }
     association :user
   end
 
-  factory :community_category do
+  factory :community_category3, class:"Community" do
     sequence(:name) { |n| "コミュニティ名_#{n}" }
     text { "コミュニティの紹介文" }
     category_id { 3 }
     association :user
   end
 
-  factory :community_with_other_introduction do
+  factory :community_with_other_name, class:"Community" do
+    name { "community name" }
+    text { "コミュニティの紹介文" }
+    category_id { 1 }
+    association :user
+  end
+
+  factory :community_with_other_introduction, class:"Community" do
     sequence(:name) { |n| "コミュニティ名_#{n}" }
-    text { "community introducion" }
+    text { "community introduction" }
     category_id { 1 }
     association :user
   end
