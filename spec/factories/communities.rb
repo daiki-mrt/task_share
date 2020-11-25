c262775ec77be5120150601a21dfdae9309759a7
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-25 21:32:57 +0900

    キーワード検索でnameも対象にする

--- spec/factories/communities.rb
+++ spec/factories/communities.rb
@@ -1,7 +1,28 @@
 FactoryBot.define do
   factory :community do
-    sequence(:name) { |n| "コミュニティタイトル_#{n}" }
-    text { Faker::Quote.matz }
+    sequence(:name) { |n| "コミュニティ名_#{n}" }
+    text { "コミュニティの紹介文" }
+    category_id { 1 }
+    association :user
+  end
+
+  factory :community_category2 do
+    sequence(:name) { |n| "コミュニティ名_#{n}" }
+    text { "コミュニティの紹介文" }
+    category_id { 2 }
+    association :user
+  end
+
+  factory :community_category do
+    sequence(:name) { |n| "コミュニティ名_#{n}" }
+    text { "コミュニティの紹介文" }
+    category_id { 3 }
+    association :user
+  end
+
+  factory :community_with_other_introduction do
+    sequence(:name) { |n| "コミュニティ名_#{n}" }
+    text { "community introducion" }
     category_id { 1 }
     association :user
   end
