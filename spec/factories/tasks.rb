a85fa86165e7e52021b5b56c48ccbe4cbe404f99
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-26 11:33:54 +0900

    taskの検索メソッドテスト

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -5,4 +5,18 @@ FactoryBot.define do
     state { 0 }
     association :user
   end
+
+  factory :completed_task, class:"Task" do
+    sequence(:title) { |n| "taskのタイトル_#{n}" }
+    sequence(:text) { Faker::Quote.matz }
+    state { 1 }
+    association :user
+  end
+
+  factory :not_completed_task, class:"Task" do
+    sequence(:title) { |n| "taskのタイトル_#{n}" }
+    sequence(:text) { Faker::Quote.matz }
+    state { 0 }
+    association :user
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,30 +5,79 @@ RSpec.describe Task, type: :model do
     @task = build(:task)
   end
 
-  context "taskを保存できる場合" do
-    it "title、text、stateがあれば保存できる" do
-      expect(@task).to be_valid
+  describe "taskの保存" do
+    context "taskを保存できる場合" do
+      it "title、text、stateがあれば保存できる" do
+        expect(@task).to be_valid
+      end
+      it "textが無くても保存できる" do
+        @task.text = nil
+        expect(@task).to be_valid
+      end
     end
-    it "textが無くても保存できる" do
-      @task.text = nil
-      expect(@task).to be_valid
+    context "taskを保存できない場合" do
+      it "titleが無いと保存できない" do
+        @task.title = nil
+        @task.valid?
+        expect(@task.errors[:title]).to include("を入力してください")
+      end
+      it "stateが無いと保存できない" do
+        @task.state = nil
+        @task.valid?
+        expect(@task.errors[:state]).to include("は一覧にありません")
+      end
+      it "userが紐付いていないと保存できない" do
+        @task.user = nil
+        @task.valid?
+        expect(@task.errors[:user]).to include("を入力してください")
+      end
     end
   end
-  context "taskを保存できない場合" do
-    it "titleが無いと保存できない" do
-      @task.title = nil
-      @task.valid?
-      expect(@task.errors[:title]).to include("を入力してください")
+
+  describe "特定のuserのtaskを取得" do
+    before do
+      @target_user = create(:user)
+      @target_user_task1 = Task.create(user_id: @target_user.id, title:"タスクタイトル1", state: 0)
+      @target_user_task2 = Task.create(user_id: @target_user.id, title:"タスクタイトル2", state: 0)
+      @not_target_user = create(:user)
     end
-    it "stateが無いと保存できない" do
-      @task.state = nil
-      @task.valid?
-      expect(@task.errors[:state]).to include("は一覧にありません")
+
+    context "一致するデータが見つかるとき" do
+      it "検索対象にしたuserが投稿したtaskを返す" do
+        expect(Task.user_is(@target_user.id)).to include(@target_user_task1, @target_user_task2)
+      end
     end
-    it "userが紐付いていないと保存できない" do
-      @task.user = nil
-      @task.valid?
-      expect(@task.errors[:user]).to include("を入力してください")
+    context "一致するデータが1件も見つからないとき" do
+      it "空のコレクションを返す" do
+        expect(Task.user_is(@not_target_user.id)).to be_empty
+      end
+    end
+  end
+
+  describe "状態を指定してtaskを検索" do
+    context "一致するデータが見つかるとき" do
+      before do
+        @completed_task1 = create(:completed_task)
+        @completed_task2 = create(:completed_task)
+        @not_completed_task1 = create(:not_completed_task)
+        @not_completed_task2 = create(:not_completed_task)
+      end
+
+      it "完了状態のtaskを返す" do
+        expect(Task.completed).to include(@completed_task1, @completed_task2)
+      end
+      it "未完了状態のtaskを返す" do
+        expect(Task.not_completed).to include(@not_completed_task1, @not_completed_task2)
+      end
+    end
+
+    context "一致するデータが1件も見つからないとき" do
+      it "完了状態のtaskがなければ、空のコレクションを返す" do
+        expect(Task.completed).to be_empty
+      end
+      it "未完了状態のtaskがなければ、空のコレクションを返す" do
+        expect(Task.not_completed).to be_empty
+      end
     end
   end
 end
