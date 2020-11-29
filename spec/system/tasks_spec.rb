80e0b1d00cf710692b8e1216e9fdf30ef77e2ca8
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-29 14:22:56 +0900

    統合テスト/タスク完了機能

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -42,4 +42,23 @@ RSpec.describe 'Tasks', type: :system do
     end
   end
 
+  describe "タスクを完了にする" do
+    before "ユーザ設定とログイン" do
+      @user = create(:user)
+      user_profile = create(:profile, user_id: @user.id)
+      sign_in_as @user
+    end
+    
+    it "完了ボタンをクリックすると、タスクが完了になり、マイページに表示されなくなる" do
+      task_title = 'タスクのタイトル'
+      fill_in 'task_title', with: task_title
+      click_on '登録'
+      expect(page).to have_content task_title
+      # 完了ボタンを押すと、そのタスクは表示されなくなり、stateカラムがtrueになる
+      click_on '完了にする'
+      expect(page).to_not have_content task_title
+      task = Task.find_by(title: 'タスクのタイトル')
+      expect(task.state).to eq true
+    end
+  end
 end
