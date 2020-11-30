4e49a303970afe61b5c06e42f3f191cffcfb13bd
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-11-30 12:46:15 +0900

    統合テスト/コミュニティ編集

--- spec/system/communities_spec.rb
+++ spec/system/communities_spec.rb
@@ -44,4 +44,42 @@ RSpec.describe 'Communities', type: :system do
     end
   end
 
+  describe "コミュニティ編集" do
+    before "ユーザ設定とログイン" do
+      @user = create(:user)
+      user_profile = create(:profile, user_id: @user.id)
+      @community = create(:community, user_id: @user.id)
+      sign_in_as @user
+    end
+
+    context "編集できるとき" do
+      it "正しく情報を入力すると、コミュニティが保存され、コミュニティ詳細ページに遷移する" do
+        # コミュニティ詳細ページへ移動する
+        visit "/communities/#{@community.id}"
+        # コミュニティ編集ページへ遷移する
+        click_link '編集する'
+        expect(current_url).to include "/communities/#{@community.id}/edit"
+        expect(page).to have_content @community.name
+        # 名前を変更するして更新ボタンを押す
+        revised_community_name = '編集後のタイトル'
+        fill_in 'community_name', with: revised_community_name
+        click_on '更新'
+        expect(page).to have_content revised_community_name
+      end
+    end
+    context "編集できないとき" do
+      it "情報が正しくないと、コミュニティが保存されず、新規作成画面に戻る" do
+        # コミュニティ詳細ページへ移動する
+        visit "/communities/#{@community.id}"
+        # コミュニティ編集ページへ遷移する
+        click_link '編集する'
+        expect(current_url).to include "/communities/#{@community.id}/edit"
+        expect(page).to have_content @community.name
+        fill_in 'community_name', with: ''
+        click_on '更新'
+        expect(page).to have_css '.error-message'
+      end
+    end
+  end
+
 end
