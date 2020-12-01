fe07eecac3094e819aa4d3d65c0870b7f89240c1
daiki-mrt <70130536+daiki-mrt@users.noreply.github.com>
2020-12-01 21:29:27 +0900

    テストコード記述

--- spec/system/answers_spec.rb
+++ spec/system/answers_spec.rb
@@ -13,4 +13,24 @@ RSpec.describe 'Answers', type: :system do
       @question = create(:question, user_id: @user.id, community_id: @community.id)
     end
 
+    context "コメント出来るとき" do
+      it "コメントを入力すると保存できて、コメント一覧エリアに表示される" do
+        # チャットページに移動する
+        visit "/communities/#{@community.id}/questions/#{@question.id}"
+        expect {
+          fill_in 'answer_text', with: '質問への回答'
+          click_on '投稿'
+          expect(page).to have_content '質問への回答'
+        }.to change { Answer.count }.by(1)
+      end
+    end
+    context "コメント出来ないとき" do
+      it "コメントを入力していないと保存できず、質問詳細ページに戻る" do
+        visit "/communities/#{@community.id}/questions/#{@question.id}"
+        expect {
+          click_on '投稿'
+        }.to change { Answer.count }.by(0)
+      end
+    end
+  end
 end
